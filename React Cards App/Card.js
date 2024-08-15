afd60265699baf18660a7092ef5175ea421522d2
EmirAliOner <emiralioner@gmail.com>
2024-08-15 14:30:51 +0300

    React Cards App
    It was developed for the purpose of playing games. This project was developed with javascript.

---
+++ React Cards App/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Card.css';
+
+export default function Card(props) {
+  return (
+  <div className='card'>
+      <div className='cardBody'>
+        <img src={props.img} />
+        <div className='about'>
+          <h2 className='cardTitle'>{props.title}</h2>
+          <p className='cardDescription'>{props.description}</p>
+        </div>
+
+      </div>
+      <button>See the Details</button>
+
+    </div>
+  )
+}
